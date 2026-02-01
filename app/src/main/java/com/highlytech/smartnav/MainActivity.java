1cb3a669906309c151a3a78d48e9978f44db201c
Aklas Linux <aklas.m8689@gmail.com>
2026-02-02 02:14:01 +0600

    Initial release of SmartNav library.

--- app/src/main/java/com/highlytech/smartnav/MainActivity.java
+++ app/src/main/java/com/highlytech/smartnav/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import com.highlytech.smartnav.sample.R;
 
 public class MainActivity extends AppCompatActivity {
 
